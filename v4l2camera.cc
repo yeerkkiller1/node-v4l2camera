ebc3629811d4e1ae19f2e8fd1c79576c1e2f697f
yeerkkiller1 <yeerkkiller1@gmail.com>
2018-07-15 23:34:28 -0400

    Test

--- v4l2camera.cc
+++ v4l2camera.cc
@@ -12,57 +12,74 @@
 using namespace v8;
 
 class MyObject : public node::ObjectWrap {
-  static Persistent<Function> constructor;
-public:
-    static void Init(Local<Object> exports) {
-        Isolate* isolate = exports->GetIsolate();
+ public:
+  static void Init(v8::Local<v8::Object> exports);
 
-        // Prepare constructor template
-        Local<FunctionTemplate> tpl = FunctionTemplate::New(isolate, New);
-        tpl->SetClassName(String::NewFromUtf8(isolate, "MyObject"));
-        tpl->InstanceTemplate()->SetInternalFieldCount(1);
+ private:
+  explicit MyObject(double value = 0);
+  ~MyObject();
 
-        // Prototype
-        NODE_SET_PROTOTYPE_METHOD(tpl, "plusOne", PlusOne);
-
-        constructor.Reset(isolate, tpl->GetFunction());
-        exports->Set(String::NewFromUtf8(isolate, "MyObject"), tpl->GetFunction());
-    }
-    
-    double value_;
-    explicit MyObject(double value = 0): value_(value) { }
-    ~MyObject() { }
-
-private:
-    static void New(const FunctionCallbackInfo<Value>& args) {
-        Isolate* isolate = args.GetIsolate();
-
-        if (args.IsConstructCall()) {
-            // Invoked as constructor: `new MyObject(...)`
-            double value = args[0]->IsUndefined() ? 0 : args[0]->NumberValue();
-            MyObject* obj = new MyObject(value);
-            obj->Wrap(args.This());
-            args.GetReturnValue().Set(args.This());
-        } else {
-            // Invoked as plain function `MyObject(...)`, turn into construct call.
-            const int argc = 1;
-            Local<Value> argv[argc] = { args[0] };
-            Local<Context> context = isolate->GetCurrentContext();
-            Local<Function> cons = Local<Function>::New(isolate, constructor);
-            Local<Object> result = cons->NewInstance(context, argc, argv).ToLocalChecked();
-            args.GetReturnValue().Set(result);
-        }
-    }
-
-    static void PlusOne(const FunctionCallbackInfo<Value>& args) {
-        Isolate* isolate = args.GetIsolate();
-
-        MyObject* obj = ObjectWrap::Unwrap<MyObject>(args.Holder());
-        obj->value_ += 1;
-
-        args.GetReturnValue().Set(Number::New(isolate, obj->value_));
-    }
+  static void New(const v8::FunctionCallbackInfo<v8::Value>& args);
+  static void PlusOne(const v8::FunctionCallbackInfo<v8::Value>& args);
+  static v8::Persistent<v8::Function> constructor;
+  double value_;
 };
+
+
+Persistent<Function> MyObject::constructor;
+
+MyObject::MyObject(double value) : value_(value) {
+}
+
+MyObject::~MyObject() {
+}
+
+void MyObject::Init(Local<Object> exports) {
+  Isolate* isolate = exports->GetIsolate();
+
+  // Prepare constructor template
+  Local<FunctionTemplate> tpl = FunctionTemplate::New(isolate, New);
+  tpl->SetClassName(String::NewFromUtf8(isolate, "MyObject"));
+  tpl->InstanceTemplate()->SetInternalFieldCount(1);
+
+  // Prototype
+  NODE_SET_PROTOTYPE_METHOD(tpl, "plusOne", PlusOne);
+
+  constructor.Reset(isolate, tpl->GetFunction());
+  exports->Set(String::NewFromUtf8(isolate, "MyObject"),
+               tpl->GetFunction());
+}
+
+void MyObject::New(const FunctionCallbackInfo<Value>& args) {
+  Isolate* isolate = args.GetIsolate();
+
+  if (args.IsConstructCall()) {
+    // Invoked as constructor: `new MyObject(...)`
+    double value = args[0]->IsUndefined() ? 0 : args[0]->NumberValue();
+    MyObject* obj = new MyObject(value);
+    obj->Wrap(args.This());
+    args.GetReturnValue().Set(args.This());
+  } else {
+    // Invoked as plain function `MyObject(...)`, turn into construct call.
+    const int argc = 1;
+    Local<Value> argv[argc] = { args[0] };
+    Local<Context> context = isolate->GetCurrentContext();
+    Local<Function> cons = Local<Function>::New(isolate, constructor);
+    Local<Object> result =
+        cons->NewInstance(context, argc, argv).ToLocalChecked();
+    args.GetReturnValue().Set(result);
+  }
+}
+
+void MyObject::PlusOne(const FunctionCallbackInfo<Value>& args) {
+  Isolate* isolate = args.GetIsolate();
+
+  MyObject* obj = ObjectWrap::Unwrap<MyObject>(args.Holder());
+  obj->value_ += 1;
+
+  args.GetReturnValue().Set(Number::New(isolate, obj->value_));
+}
+
 
 
 void InitAll(Local<Object> exports, Local<Object> module) {
